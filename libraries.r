76780b6238e3b4134a0905acad985334c8d031f3
Marco Baldo <62661915+marcobaldoroma@users.noreply.github.com>
2020-07-18 22:57:32 +0200

    Update libraries.r

--- libraries.r
+++ libraries.r
@@ -1,23 +1,22 @@
 install.packages(c("sp", "GGally", "raster", "rgdal", "ggplot2", "devtools", "vegan", "spatstat", "RStoolbox", "rasterdiv", "rasterVis",  "sf", "ncdf4", "sdm" ))
 
+# to download MODIS images directly on R 
 install.packages("MODIStsp") # MODIS dataset
 install.packages("gWidgetsRGtk2") #necessary for GTK+ library 
 
 library(devtools)
-install_github("ropensci/MODIStsp") # aggiornamento
-
-
-
-
+install_github("ropensci/MODIStsp") # aggiornamento  dai un occhiata sul cran e google
 library(gWidgetsRGtk2)
 
+library(MODISTools) #install.packages("MODISTools")
 
+# libraries
 library(sp)
 library(GGally)
 library(raster)
 library(rgdal)    ##library(gdalUtils) to traslate file format
 library(ggplot2)
-library("devtools")
+library(devtools)
 library(vegan)
 library(spatstat)
 library(RStoolbox)
